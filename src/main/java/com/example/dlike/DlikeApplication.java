3f59bb4e40fd3d7baf047fba8502f995121d15e2
jhs512 <jangka512@gmail.com>
2023-06-04 20:52:42 +0900

    setting

---
+++ src/main/java/com/example/dlike/DlikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dlike/DlikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dlike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
